57e1250e12037c134fd9e030787aaee0bfaac668
Sushil1-0 <sb.bs014@gmail.com>
2024-05-01 17:18:37 +0900

    add CRUD operation

--- src/main/java/jp/smartsolar/Employee.java
+++ src/main/java/jp/smartsolar/Employee.java
@@ -1,16 +1,21 @@
 package jp.smartsolar;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 
 @Entity
-
+@Table(name = "employee")
 public class Employee {
+    public Employee() {
+    }
+
+    public Employee( String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
--- src/main/java/jp/smartsolar/Main.java
+++ src/main/java/jp/smartsolar/Main.java
@@ -2,6 +2,7 @@ package jp.smartsolar;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
 public class Main {
@@ -9,18 +10,32 @@ public class Main {
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
 
+        Employee employee = new Employee("Desolated", "Hell");
+        entityManager.persist(employee);
+        entityTransaction.commit();
+        System.out.println("Employee created with ID: " + employee.getId());
 
+        // Read
+        Employee foundEmployee = entityManager.find(Employee.class, employee.getId());
+        System.out.println("Employee found: " + foundEmployee);
 
-        // Finding an Employee entity by its primary key
-        Employee employee = entityManager.find(Employee.class, 1);
+        // Update
+        entityTransaction.begin();
+        foundEmployee.setName("Jane Doe");
+        entityManager.merge(foundEmployee);
+        entityTransaction.commit();
+        System.out.println("Employee updated: " + foundEmployee);
 
-        if (employee != null) {
-            System.out.println("Found Employee: " + employee.getName());
-        } else {
-            System.out.println("Employee not found");
-        }
+        // Delete
+        entityTransaction.begin();
+        entityManager.remove(foundEmployee);
+        entityTransaction.commit();
+        System.out.println("Employee deleted");
 
+        entityManager.close();
         entityManagerFactory.close();
 
     }
